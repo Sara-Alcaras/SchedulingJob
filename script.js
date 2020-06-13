cdb9a33362027ce310a8ed9b537d20fa7e0e50e8
Sara-Alcaras <saraalcaras401@gmail.com>
2020-06-12 21:52:12 -0300

    creating variables

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ var Job = [
     { "ID": 2, "Description": "Importação de dados da Base Legada", "MaxDate": "2019-11-11 12:00:00,", "time": 4 }, 
     { "ID": 3, "Description": "Importação de dados de integração", "MaxDate": "2019-11-11 08:00:00", "time": 6 }
 ]
-
+// console.log(Job[0]);
 var lista = 0;
 
-console.log(Job[0]); 
+const dateStart = new Date(' 2019-11-10 09:00:00');
 
-//Verificar urgencia de serv
+const dateEnd = new Date('2019-11-11 12:00:00');
+
+for (i = 0; i < Job.length; i++) {
+    var dateIndex = Job[i].MaxDate;
+    console.log(Job[i].MaxDate)
+    if (dateStart.getTime() <= new Date(Job[i].MaxDate).getTime() 
+    && dateEnd.getTime() >= new Date(Job[i].MaxDate).getTime()) {
+        console.log('A data ', dateIndex, ' está contida dentro do período especificado');
+    } else {
+        console.log('A data ', Job[i].MaxDate, ' está fora do período especificado');
+    }
+}
+
 for (i = 0; i < Job.length - 1; i++) {
-    if (Job[i].MaxDate.getTime > Job[i + 1].MaxDate.getTime)
-        Lista = i;
-    console.log("Depois de comparar", i, "com", i + 1, lista);
+    if (new Date(Job[i].MaxDate).getTime() < new Date(Job[i + 1].MaxDate).getTime())
+        lista = i;
+    console.log('Na iteração', i, 'Lista equivale', lista);
 } 
