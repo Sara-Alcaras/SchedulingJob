0fadf760f0ddece51d27969eb43f1d9a528f1a2a
Sara-Alcaras <saraalcaras401@gmail.com>
2020-06-14 13:38:54 -0300

    output change

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-var Job = [
-    { 
-    "ID": 1, 
-    "Description": "Importação de arquivos de fundos", 
-    "MaxDate": "2019-11-10 12:00:00,",
-    "time": 2 
-    }, 
+var Job = [{ "ID": 1, "Description": "Importação de arquivos de fundos", "MaxDate": "2019-11-10 08:00:00", "time": 2 }, { "ID": 2, "Description": "Importação de dados da Base Legada", "MaxDate": "2019-11-11 12:00:00", "time": 4 }, { "ID": 3, "Description": "Importação de dados de integração", "MaxDate": "2019-11-11 08:00:00", "time": 6 }]
+    // // verificar se está correto
+    // console.log(Job[0]);
+var array;
 
-    { 
-    "ID": 2, 
-    "Description": 
-    "Importação de dados da Base Legada", 
-    "MaxDate": "2019-11-11 12:00:00,", 
-    "time": 4 
-    }, 
+const dateStart = new Date(' 2019-11-10 08:00:00');
 
-    { 
-    "ID": 3, 
-    "Description": "Importação de dados de integração", 
-    "MaxDate": "2019-11-11 08:00:00", 
-    "time": 6 
-    }
-
-]
-// verificar se está correto
-// console.log(Job[0]);
-
-var lista = 0;
+const dateEnd = new Date('2019-11-11 12:00:00');
+var count = 0;
 
 for (i = 0; i < Job.length; i++) {
-    
+    var dateIndex = Job[i].MaxDate;
+    console.log(Job[i].MaxDate)
+    if (dateStart.getTime() <= new Date(Job[i].MaxDate).getTime() 
+    && dateEnd.getTime() >= new Date(Job[i].MaxDate).getTime()) {
+        console.log('A data', dateIndex, 'está contida dentro do período especificado');
+    } else {
+        console.log('A data', Job[i].MaxDate, 'está fora do período especificado');
+    }
 }
 
 console.log('Consultando array antes de ordenar');
-console.log(Job[0].MaxDate);
-console.log(Job[1].MaxDate);
-console.log(Job[2].MaxDate);
+for (i = 0; i < Job.length; i++) {
+    console.log(Job[i].MaxDate, Job[i].ID);
+}
 
-
+//Ordenar array pela data
 Job.sort(function(a, b) {
     var dateA = new Date(a.MaxDate),
         dateB = new Date(b.MaxDate);
     return dateA - dateB;
 });
+
 console.log('Consultando array depois de ordenar');
-console.log(Job[0].MaxDate);
-console.log(Job[1].MaxDate);
-console.log(Job[2].MaxDate); 
+for (i = 0; i < Job.length; i++) {
+    console.log(Job[i].MaxDate, Job[i].ID);
+}
+
+console.log('Imprimindo agenda');
+
+
+for (i = 0; i < Job.length; i++) {
+
+    count = count + Job[i].time;
+    var print = Job[i].ID
+    document.write(print);
+
+    if (count == 8) {
+        // Pular linha
+        document.writeln(' <br/>');
+        count = 0;
+    }
+}
