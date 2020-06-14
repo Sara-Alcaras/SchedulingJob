685f423deb35e479b6f578ed6647e1a1005665a0
Sara-Alcaras <saraalcaras401@gmail.com>
2020-06-14 12:57:50 -0300

    querying data

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 var Job = [
-    { "ID": 1, "Description": "Importação de arquivos de fundos", "MaxDate": "2019-11-10 12:00:00,", "time": 2 }, 
-    { "ID": 2, "Description": "Importação de dados da Base Legada", "MaxDate": "2019-11-11 12:00:00,", "time": 4 }, 
-    { "ID": 3, "Description": "Importação de dados de integração", "MaxDate": "2019-11-11 08:00:00", "time": 6 }
+    { 
+    "ID": 1, 
+    "Description": "Importação de arquivos de fundos", 
+    "MaxDate": "2019-11-10 12:00:00,",
+    "time": 2 
+    }, 
+
+    { 
+    "ID": 2, 
+    "Description": 
+    "Importação de dados da Base Legada", 
+    "MaxDate": "2019-11-11 12:00:00,", 
+    "time": 4 
+    }, 
+
+    { 
+    "ID": 3, 
+    "Description": "Importação de dados de integração", 
+    "MaxDate": "2019-11-11 08:00:00", 
+    "time": 6 
+    }
+
 ]
+
 // console.log(Job[0]);
 var lista = 0;
 
-const dateStart = new Date(' 2019-11-10 09:00:00');
+const dateStart = new Date('2019-11-10 09:00:00');
 
 const dateEnd = new Date('2019-11-11 12:00:00');
 
@@ -15,9 +35,9 @@ for (i = 0; i < Job.length; i++) {
     console.log(Job[i].MaxDate)
     if (dateStart.getTime() <= new Date(Job[i].MaxDate).getTime() 
     && dateEnd.getTime() >= new Date(Job[i].MaxDate).getTime()) {
-        console.log('A data ', dateIndex, ' está contida dentro do período especificado');
+        console.log('A data', dateIndex, 'está contida dentro do período especificado');
     } else {
-        console.log('A data ', Job[i].MaxDate, ' está fora do período especificado');
+        console.log('A data', Job[i].MaxDate, 'está fora do período especificado');
     }
 }
 
